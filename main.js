4a76a1982c053bb1301aa392cc490d4ea64737d2
harenPy11a <112649227+harenPy11a@users.noreply.github.com>
2022-10-25 14:31:05 -0500

    seasaw

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ var engine = Engine.create()
 var render = Render.create({
     element:document.body,
     engine:engine,
-    options:{ 
+    options:{
         hasBounds : true,
         width: innerWidth,
         height: innerHeight
@@ -30,7 +30,7 @@ Runner.run(runner, engine)
 var cannonside1 = Bodies.rectangle(90, 410, 300, 20, {isStatic:true})
 var cannonside2 = Bodies.rectangle(120, 480, 300, 20, {isStatic:true})
 var end = Bodies.rectangle(10, 520, 45, 45, {isStatic:true})
-var ball = Bodies.polygon(170, 390, 25, 25, {restitution: 0.9, mass:5})
+var ball = Bodies.polygon(170, 390, 25, 25, {restitution: 0.9, friction: 0, mass: 10})
 Body.rotate(cannonside1, 2.5);
 Body.rotate(cannonside2, 2.5)
 Body.rotate(end, 2.5)
@@ -39,23 +39,23 @@ Render.lookAt(render, {
     max: { x: ball.position.x + 750, y: ball.position.y+300}
 });  
 // create funnel
-var funnel1 = Bodies.rectangle(1075, 780, 80, 20, {isStatic:true});
-var funnel2 = Bodies.rectangle(925, 780, 80, 20, {isStatic:true});
-var funnel3 = Bodies.rectangle(1070, 840, 80, 20, {isStatic:true});
-var funnel4 = Bodies.rectangle(930, 840, 80, 20, {isStatic:true});
-var funnel5 = Bodies.rectangle(1065, 890, 80, 20, {isStatic:true});
-var funnel6 = Bodies.rectangle(935, 890, 80, 20, {isStatic:true});
-var funnel7 = Bodies.rectangle(1060, 940, 80, 20, {isStatic:true});
-var funnel8 = Bodies.rectangle(940, 940, 80, 20, {isStatic:true});
+var funnel1 = Bodies.rectangle(1075, 780, 80, 20, {isStatic:true, angle: -1.3});
+var funnel2 = Bodies.rectangle(925, 780, 80, 20, {isStatic:true, angle:1.3});
+var funnel3 = Bodies.rectangle(1070, 840, 80, 20, {isStatic:true, angle:-1.2});
+var funnel4 = Bodies.rectangle(930, 840, 80, 20, {isStatic:true, angle:1.2});
+var funnel5 = Bodies.rectangle(1065, 890, 80, 20, {isStatic:true, angle:-1.1});
+var funnel6 = Bodies.rectangle(935, 890, 80, 20, {isStatic:true, angle:1.1});
+var funnel7 = Bodies.rectangle(1060, 940, 80, 20, {isStatic:true, angle:-1.0});
+var funnel8 = Bodies.rectangle(940, 940, 80, 20, {isStatic:true, angle:1.0});
 
-Body.rotate(funnel1,-1.3);
-Body.rotate(funnel2,1.3);
-Body.rotate(funnel3,-1.2);
-Body.rotate(funnel4,1.2);
-Body.rotate(funnel5,-1.1);
-Body.rotate(funnel6,1.1);
-Body.rotate(funnel7,-1.0);
-Body.rotate(funnel8,1.0);
+// Body.rotate(funnel1,);
+// Body.rotate(funnel2,);
+// Body.rotate(funnel3,);
+// Body.rotate(funnel4,);
+// Body.rotate(funnel5,);
+// Body.rotate(funnel6,);
+// Body.rotate(funnel7,);
+// Body.rotate(funnel8,);
 //create pegs for plinkos
 var cols = 10;
 var rows = 10;
@@ -84,71 +84,75 @@ for(var i = 0; i < rows; i++){
     }
 }
 //creates the boundaries for plinkos
-var boundary1 = Bodies.rectangle(175, 2050, 1450, 20, {isStatic:true});
-var boundary2 = Bodies.rectangle(1700, 2050, 1450, 20, {isStatic:true});
+var boundary1 = Bodies.rectangle(175, 2050, 1450, 20, {isStatic:true, angle: Math.PI/2});
+var boundary2 = Bodies.rectangle(1700, 2050, 1450, 20, {isStatic:true, angle: Math.PI/2});
 
-Body.rotate(boundary1,Math.PI/2)
-Body.rotate(boundary2,Math.PI/2)
+// Body.rotate(boundary1,)
+// Body.rotate(boundary2,)
+
 
 //bouncy walls
-var wall1 = Bodies.rectangle(1050, 130, 50, 150, {isStatic:true})
-var wall2 = Bodies.rectangle(750, 260, 50, 150, {isStatic:true})
-var wall3 = Bodies.rectangle(1050, 330, 50, 150, {isStatic:true})
-var wall4 = Bodies.rectangle(760, 430, 50, 150, {isStatic:true})
-Body.rotate(wall1,-2.5)
-Body.rotate(wall2,2.5)
-Body.rotate(wall3,-2.5)
-Body.rotate(wall4,2.5) 
+var wall1 = Bodies.rectangle(1050, 130, 50, 150, {isStatic:true, angle:-2.5, restitution:1.3})
+var wall2 = Bodies.rectangle(780, 250, 50, 150, {isStatic:true, angle:2.5, restitution:1.3})
+var wall3 = Bodies.rectangle(1080, 350, 50, 150, {isStatic:true, angle:-2.5, restitution:1.3})
+var wall4 = Bodies.rectangle(780, 430, 50, 150, {isStatic:true, angle:2.5, restitution:1.3})
+// Body.rotate(wall1,)
+// Body.rotate(wall2,2.5)
+// Body.rotate(wall3,-2.5)
+// Body.rotate(wall4,2.5) 
 //create the ramps
-var platform = Bodies.rectangle(1420, 2880, 580, 20, {isStatic:true, friction: -10})
-var platform2 = Bodies.rectangle(1000, 3060, 300, 20, {isStatic:true, friction: -10})
-var platform3 = Bodies.rectangle(700, 3140, 300, 20, {isStatic:true, friction: -10})
-var platform4 = Bodies.rectangle(120, 3070, 300, 20, {isStatic:true, friction: -10})
-var platform5 = Bodies.rectangle(320, 3300, 300, 20, {isStatic:true, friction: -10})
-var platform6 = Bodies.rectangle(580, 3435, 300, 20, {isStatic:true, friction: -10})
-var platform7 = Bodies.rectangle(1070, 3500, 700, 20, {isStatic:true, friction: 0})
-Body.rotate(platform,-.45)
-Body.rotate(platform2, -0.3)
-Body.rotate(platform3, -.3)
-Body.rotate(platform4,-2.2)
-Body.rotate(platform5,.7)
-Body.rotate(platform6,.2)
-Body.rotate(platform7,.1)
-//Seasaw 
+var platform = Bodies.rectangle(1420, 2880, 580, 20, {isStatic:true, friction: 0, restitution: 0.8, angle: -.45})
+var platform2 = Bodies.rectangle(1000, 3040, 300, 20, {isStatic:true, friction: 0, restitution: 0.8, angle: -.3})
+var platform3 = Bodies.rectangle(700, 3140, 300, 20, {isStatic:true, friction: 0, restitution: 0.8, angle: -.3})
+var platform4 = Bodies.rectangle(120, 3070, 300, 20, {isStatic:true, friction: 0, restitution: 0.8, angle: -2.2})
+var platform5 = Bodies.rectangle(320, 3300, 300, 20, {isStatic:true, friction: 0, restitution: 0.8, angle: .7})
+var platform6 = Bodies.rectangle(580, 3435, 300, 20, {isStatic:true, friction: 0, restitution: 0.8, angle: .2})
+var platform7 = Bodies.rectangle(1070, 3500, 700, 20, {isStatic:true, friction: 0, restitution: 0.8, angle: .1})
+// Body.rotate(platform,-.45)
+// Body.rotate(platform2, -0.2)
+// Body.rotate(platform3, -.3)
+// Body.rotate(platform4,-2.2)
+// Body.rotate(platform5,.7)
+// Body.rotate(platform6,.2)
+// Body.rotate(platform7,.1)
+
+//add reverse gravity tunnel;
+var base = Bodies.rectangle(2000, 3970, 800, 20, {isStatic:true, friction:0, angle: .2})
+var tunnelLeft = Bodies.rectangle(2300, 2400, 15, 3070, {isStatic:true})
+var tunnelRight = Bodies.rectangle(2400, 2420, 15, 3200, {isStatic:true})
+// seasaw
 var baseOfSeasaw = Bodies.rectangle(2450, 450, 500, 15, {isStatic:true, mass: .05})
 var seaSaw = Bodies.rectangle(2450, 550, 500, 15, {isStatic:false})
 var block = Bodies.rectangle(2670, 620, 50, 50, {isStatic:true, mass:.000001})
-//add reverse gravity tunnel;
-var base = Bodies.rectangle(2000, 4000, 750, 20, {isStatic:true, friction:10})
-var tunnelLeft = Bodies.rectangle(2300, 2400, 15, 3070, {isStatic:true})
-var tunnelRight = Bodies.rectangle(2400, 2420, 15, 3200, {isStatic:true, mass: 1})
 
 Composite.add(engine.world, [cannonside1, cannonside2, end, ball, wall1, wall2, wall3, wall4,
-funnel1, funnel2,funnel3,funnel4,funnel5,funnel6,funnel7,funnel8,boundary1,boundary2,
-platform, platform2, platform3,platform4, platform5, platform6, platform7, base, tunnelLeft, tunnelRight, baseOfSeasaw, seaSaw, block,
+    funnel1, funnel2,funnel3,funnel4,funnel5,funnel6,funnel7,funnel8,boundary1,boundary2,
+    platform, platform2, platform3,platform4, platform5, platform6, platform7, base, tunnelLeft, tunnelRight, baseOfSeasaw, seaSaw, block,
 
-    Constraint.create({ bodyA: seaSaw, pointB: { x: 2450, y: 550 }}) 
+    Constraint.create({ bodyA: seaSaw, pointB: { x: 2350, y: 450 }}), 
+    Constraint.create({ bodyA: seaSaw, pointB: { x: 2550, y: 450 }}) 
 
 ])
-for(var i =0;i<pegs.length;i++)(
-    Composite.add(engine.world,[pegs[i]])
-)
+    for(var i =0;i<pegs.length;i++){
+        Composite.add(engine.world,[pegs[i]])
+    }
 //create reverse gravity tunnel
+function setup(){
+
+}
 setTimeout(() => {
     fireCannon();
-}, 3000);
+}, 3600);
 
 //code for changing camera view
 // Render.lookAt(render, {
 //     min: { x: ____, y: _________},
 //     max: { x: ______, y: __________}
 // });
-function setup(){
 
-}
 function fireCannon(){
     console.log("fired")
-    Body.applyForce(ball, ball.position, {x: .35, y:-.35})
+    Body.applyForce(ball, ball.position, {x: 0.75, y:-0.75})
     followCamera(ball)
 }
 //add a way to zoom in and out with scrolll wheeeeell
@@ -165,42 +169,20 @@ function followCamera(b){
         if(Collision.collides(b, tunnelRight)){
             engine.gravity.y = -1;
             engine.gravity.x = 0;
-            // tunnelRight.isStatic = false;
             block.isStatic = false;
         }
-        if(Collision.collides(b,platform)){
-            engine.gravity.x = -0.5;
-            Body.torque(ball, 1)
-        }
-        if(Collision.collides(b,platform2)){
-            engine.gravity.x = -0.25;
-            Body.torque(ball, 1)
-        }
-        if(Collision.collides(b,platform3)){
-            engine.gravity.x = -0.25;
-            Body.torque(ball, 1)
-        }
-        if(Collision.collides(b,platform4)){
-            engine.gravity.x = 0.25;
-            Body.torque(ball, 1)
-        }
-        if(Collision.collides(b,platform5)){
-            engine.gravity.x = 0.25;
-            Body.torque(ball, 1)
-        }
-        if(Collision.collides(b,platform6)){
-            engine.gravity.x = 0.25;
-            Body.torque(ball, 1)
-        }
         if(Collision.collides(b,platform7)){
-            engine.gravity.x = 0.65;
-            Body.torque(ball, 1)
+            // engine.gravity.x = 0.65;
+            Body.applyForce(ball, ball.position, {x: 0.005, y:0})
+            
+        }
+        if(Collision.collides(b,seaSaw)){
+            Composite.remove(engine.world,[ball])
+            followCamera(block)
         }
         //clear interval and re-call function whenever you want to follow a different object.
 
     }, 1)  
 }
-
-
 
 
